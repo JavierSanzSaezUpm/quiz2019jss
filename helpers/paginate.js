83e2bbb7c41f4c243409cb8490e5c62a52e4c5dc
Javier Sanz Sáez <javier.sanz.saez@alumnos.upm.es>
2019-06-03 02:06:27 +0200

    Finishing up!

--- helpers/paginate.js
+++ helpers/paginate.js
@@ -30,6 +30,8 @@ const paginate = (totalItems, itemsPerPage, currentPage, url, param_name) => {
 
     const html = [];
 
+    html.push('<ul class="pagination">');
+
     // Modify neighbors to avoid having few buttons:
     //  - If there is no space for the neighbors on the left, I show more by the right.
     //  - If there is no space for the neighbors on the right, I show more by the left.
@@ -42,40 +44,42 @@ const paginate = (totalItems, itemsPerPage, currentPage, url, param_name) => {
     // First page
     if (1 < currentPage - neighbours) {
         url = addPagenoToUrl(url, 1, param_name);
-        html.push('<a href="' + url + '"> ' + 1 + ' </a>');
+        html.push('<li> <a href="' + url + '">' + 1 + '</a></li>');
     }
 
     // Previous pages: between the first page and the middle pages
     if (currentPage - neighbours > 2) {
         const n = Math.trunc(( 1 + currentPage - neighbours) / 2);
         url = addPagenoToUrl(url, n, param_name);
-        html.push('<a href="' + url + '"> ' + n + ' </a>');
+        html.push('<li> <a href="' + url + '">' + n + '</a></li>');
     }
 
     // Pages in the middle
     for (let i = 1; i <= total; i++) {
         if (i === currentPage) {
-            html.push('<a class="active" href="#"> ' + i + ' </a>');
+            html.push('<li class="active"> <a href="#">' + i + '</a></li>');
         } else {
             if (i >= currentPage - neighbours && i <= currentPage + neighbours) {
                 url = addPagenoToUrl(url, i, param_name);
-                html.push('<a href="' + url + '"> ' + i + ' </a>');
+                html.push('<li> <a href="' + url + '">' + i + '</a></li>');
             }
         }
     }
 
-    // Next pages: between the middle pages and the last page
+    // Next pages: betwenn the middle pages and the last page
     if (currentPage + neighbours < total - 1) {
         const n = Math.trunc(( total + currentPage + neighbours + 1) / 2);
         url = addPagenoToUrl(url, n, param_name);
-        html.push('<a href="' + url + '">' + n + '</a>');
+        html.push('<li> <a href="' + url + '">' + n + '</a></li>');
     }
 
     // Last page
     if (total > currentPage + neighbours) {
         url = addPagenoToUrl(url, total, param_name);
-        html.push('<a href="' + url + '"> ' + total + ' </a>');
+        html.push('<li> <a href="' + url + '">' + total + '</a></li>');
     }
+
+    html.push('</ul>');
 
     return html.join('');
 };
